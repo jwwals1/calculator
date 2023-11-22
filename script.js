02bae3fdf358e54adb9e7d166ec6bed403dc3fd5
jwwals1 <joe_walsh59@yahoo.com>
2023-11-22 11:58:07 -0600

    started userInput function

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("test")
+})
+
+const numbers = document.querySelectorAll(".number")
+
+function userInput(num) {
+    numberInput = num
+    console.log(num)
+}
